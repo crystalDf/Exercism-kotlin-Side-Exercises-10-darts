2c74a21f34bd01709b94456ccb57db091674cfb6
crystalDf <chendong333@gmail.com>
2020-02-19 22:55:14 +0800

    10 darts

---
+++ src/main/kotlin/Darts.kt
@@ -0,0 +1,14 @@
+import kotlin.math.ceil
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+object Darts {
+
+    fun score(x: Number, y: Number) =
+            when (ceil(sqrt(x.toDouble().pow(2) + y.toDouble().pow(2))).toInt()) {
+                in 0..1 -> 10
+                in 2..5 -> 5
+                in 6..10 -> 1
+                else -> 0
+            }
+}
